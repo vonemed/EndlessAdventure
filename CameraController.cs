172b7da31bfe90d12a50f2630655aa27ec2a50a4
polezniy <glebadem@gmail.com>
2019-09-13 12:07:28 +0300

    Initial Commit

---
+++ CameraController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float cameraSpeed;
+
+    protected Vector3 cameraPos;
+
+    void Update()
+    {
+        cameraPos = transform.position;
+        Debug.Log(cameraSpeed);
+        if (Input.GetKey(KeyCode.W))
+        {
+            cameraPos.x += cameraSpeed * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+        }
+
+        transform.position = cameraPos;
+    }
+}
