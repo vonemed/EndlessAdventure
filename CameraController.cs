17c9b42252a58e2c1d88aae103137e321e8f1ad0
polezniy <glebadem@gmail.com>
2019-09-13 14:07:31 +0300

    Added basic camera movement

--- CameraController.cs
+++ CameraController.cs
@@ -5,32 +5,52 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public float cameraSpeed;
+    public float rotationSpeed;
 
-    protected Vector3 cameraPos;
+    private Vector3 cameraPos; // Current position of camera
+    private Vector3 frwdDir; // Z axis of camera in world space
+    private Vector3 rightDir; // X axis of camera in world space
 
     void Update()
     {
         cameraPos = transform.position;
-        Debug.Log(cameraSpeed);
+        frwdDir = transform.forward;
+        rightDir = transform.right;
+
+        #region CameraMovement
         if (Input.GetKey(KeyCode.W))
         {
-            cameraPos.x += cameraSpeed * Time.deltaTime;
+            cameraPos += (frwdDir * cameraSpeed) * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-
+            cameraPos -= (rightDir * cameraSpeed) * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.S))
         {
-
+            cameraPos -= (frwdDir * cameraSpeed) * Time.deltaTime;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-
+            cameraPos += (rightDir * cameraSpeed) * Time.deltaTime;
         }
+        #endregion
+        #region CameraRotation
+
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(transform.up, -rotationSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(transform.up, rotationSpeed * Time.deltaTime);
+        }
+
+        #endregion
 
         transform.position = cameraPos;
     }
