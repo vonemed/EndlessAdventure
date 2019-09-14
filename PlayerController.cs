d01e0678979b8e9362abd438ad8928f15b5570eb
polezniy <glebadem@gmail.com>
2019-09-14 11:30:06 +0300

    Added basic raycasting

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public LayerMask ground;
+    Camera cam;
+
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, ground))
+            {
+                Debug.Log(hit.transform);
+            }
+        }
+    }
+}
