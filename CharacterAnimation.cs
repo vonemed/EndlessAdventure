af24fce356eacfd7c2a9af04e8f0ddc251a8abe8
polezniy <glebadem@gmail.com>
2019-09-17 14:42:08 +0300

    Added character animation

---
+++ CharacterAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    const float locomotionAnimationSmoothTime = .1f;
+
+    NavMeshAgent agent;
+    Animator animator;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed; // Current speed divided by its maximum speed
+        animator.SetFloat("speedPercent", speedPercent, locomotionAnimationSmoothTime, Time.deltaTime);
+    }
+}
